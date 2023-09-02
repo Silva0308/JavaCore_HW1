e1d9ffad4e1d56560aa401eb626afec7da6100fc
Silva0308 <v.travinina@gmail.com>
2023-09-02 12:40:48 +0300

    Some changes

--- src/main/java/ru/gb/converter/Fahrenheit.java
+++ src/main/java/ru/gb/converter/Fahrenheit.java
@@ -1,2 +1,15 @@
-package ru.gb.converter;public class Fahrenheit {
+package ru.gb.converter;
+
+/**Чтобы преобразовать градус Фаренгейта в градус Цельсия,
+ * вычтите 32 из температуры Фаренгейта, а затем разделите свой ответ на 1,8. */
+public class Fahrenheit {
+    /**
+     * Функция преобразования
+     *
+     * @param temperature число, требующее перевода
+     * @return результат.
+     */
+    public float convert(float temperature) {
+        return (temperature - 32) * 5/9;
+    }
 }
